5f172eaf84ce99e59bc0bdb36499857c9ea0d950
Amaanshk06 <amaanshk234@gmail.com>
2025-05-26 16:39:29 +0530

    initial commit

--- src/components/Video.js
+++ src/components/Video.js
@@ -2,18 +2,17 @@ import React, { useState } from 'react';
 
 function Video() {
   const [isOpen, setIsOpen] = useState(false);
-  const videoId = 'xcWIX0uGv7I'; // Replace with your YouTube video ID
-  const thumbnailUrl = `/assets/images/Video-img/video-thumbnail.WEBP`;
+  const videoId = 'xcWIX0uGv7I';
+  const thumbnailUrl = '/assets/images/Video-img/video-thumbnail.WEBP';
 
   const openModal = () => setIsOpen(true);
   const closeModal = () => setIsOpen(false);
 
   return (
-    <>
+    <div className="video-wrapper" id="video">
       <div
         className="video-section"
-        style={{ backgroundImage: `url(${thumbnailUrl})` ,backgroundSize:'cover' }}
-        id='video'
+        style={{ backgroundImage: `url(${thumbnailUrl})` }}
       >
         <button className="play-button" onClick={openModal}>
           <img
@@ -22,24 +21,24 @@ function Video() {
             className="play-icon"
           />
         </button>
-      </div>
 
-      {isOpen && (
-        <div className="video-modal-overlay" onClick={closeModal}>
-          <div className="video-modal-content" onClick={e => e.stopPropagation()}>
-            <button className="close-button" onClick={closeModal}>×</button>
-            <iframe
-              className="youtube-iframe"
-              src={`https://www.youtube.com/embed/${videoId}?autoplay=1`}
-              title="YouTube Video"
-              frameBorder="0"
-              allow="autoplay; encrypted-media"
-              allowFullScreen
-            />
+        {isOpen && (
+          <div className="video-modal-overlay" onClick={closeModal}>
+            <div className="video-modal-content" onClick={(e) => e.stopPropagation()}>
+              <button className="close-button" onClick={closeModal}>×</button>
+              <iframe
+                className="youtube-iframe"
+                src={`https://www.youtube.com/embed/${videoId}?autoplay=1`}
+                title="YouTube video"
+                frameBorder="0"
+                allow="autoplay; encrypted-media"
+                allowFullScreen
+              />
+            </div>
           </div>
-        </div>
-      )}
-    </>
+        )}
+      </div>
+    </div>
   );
 }
 
